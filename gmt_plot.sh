329dc7f1b33c0d49aaabe5d2239b5d5e00adb47b
Constantine VI <constantinevi@outlook.com>
2018-08-20 11:38:12 +0800

    改變座標軸預設設定

--- gmt_plot.sh
+++ gmt_plot.sh
@@ -206,6 +206,10 @@ function config_default_v(){
     Map_Projection=M
     Map_Width=6
     Map_Offset_X=4
+    Map_Bax=1
+    Map_Bbx=0.2
+    Map_Bay=1
+    Map_Bby=0.2
     scale_Label="LOS Velocity (mm/yr)"
 }
 
